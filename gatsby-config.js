146537497d0fe0d570338b3eb297c601ac04f097
Darryl Frost <darryl.frost@gmail.com>
2020-09-20 22:44:28 +0100

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,38 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: "Test Title",
+    description: "JavaScript, React, GraphQL and Gatsby",
+    author: "Ibaslogic",
+  },
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-transformer-remark",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`,
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 750,
+              linkImagesToOriginal: false,
+            },
+          },
+        ],
+      },
+    },
+  ],
 }
---
+++ src/pages/contact.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Layout from '../components/layout'
+import Metadata from "../components/metadata"
+
+const Contact = () => {
+  return (
+    <Layout>
+      <Metadata title="Contact" description="This is the contact page"/>
+      <div>
+        <h1>Contact Page</h1>
+        <p>You can find Ibas on Twitter via @ibaslogic</p>
+      </div>
+    </Layout>
+
+  )
+}
+
+export default Contact
